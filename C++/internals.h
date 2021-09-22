9ac6f039ad99f44248a81274639b130edff4695b
Divyanshu <40380497+0xDivyanshu@users.noreply.github.com>
2021-09-22 23:45:54 +0530

    Create internals.h

---
+++ C++/internals.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<string>
+#include <iostream>
+#include<windows.h>
+#include<atlconv.h>
+#include <stdlib.h>
+#include<openssl/md5.h>
+#include<winhttp.h>
+#include<TlHelp32.h>
+#include<Psapi.h>
+#include<winternl.h>
+#include<stdlib.h>
+#include <Winternl.h>
